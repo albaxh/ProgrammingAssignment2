88114797adc8a8296f52288fb5d5bf04c1676242
Alba Xhani <alba.xhani@gmail.com>
2021-03-03 13:59:52 +0100

    add implementation for makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+    invertedM <- NULL
+    set <- function(n) {
+        m <<- n
+        invertedM <<- NULL
+    }
+    get <- function() m
+    setInvertedMatrix <- function(inverted) invertedM <<- inverted
+    getInvertedMatrix <- function() invertedM
+    list(set = set, get = get,
+         setInvertedMatrix = setInvertedMatrix,
+         getInvertedMatrix = getInvertedMatrix)
 }
 
 
